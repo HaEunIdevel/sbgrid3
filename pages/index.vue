0669a5ee80748396e4dd1ff238d79df847de671e
HaEunIdevel <hekim@idevel.kr>
2023-08-03 17:13:53 +0900

    그리드 세팅, select와focus사용

--- pages/index.vue
+++ pages/index.vue
@@ -7,46 +7,6 @@ import axios from "axios";
 export default {
   setup() {
     const jsonData = ref(null);
-
-    let dsconfig = {
-      data: jsonData.value,
-      groups: [{ field: "class", isOpen: true }],
-    };
-
-    let gridConfig = {
-      dataSource: dsconfig,
-      container: "#grid",
-      width: "40%",
-      height: "400px",
-
-      columns: [
-        { field: "class", width: 80 },
-        { field: "name" },
-        { field: "number" },
-        {
-          field: "korean",
-          groups: [
-            {
-              group: "*",
-              aggregates: [{ func: "total", payload: "name" }],
-              header: { prefix: "합계: ", field: "total" },
-            },
-          ],
-          //   width: 50,
-        },
-        { field: "english", width: 50 },
-        { field: "math", width: 50 },
-      ],
-      groupable: true,
-      groupAutoOpen: true,
-    };
-    // 데이터 변경을 감지하고 그에 따라 그리드를 생성
-    watch(jsonData, () => {
-      gridConfig.dataSource = jsonData.value;
-      const datagrid = SBGrid3.createGrid(gridConfig);
-      datagrid.refresh();
-    });
-
     onMounted(async () => {
       try {
         const response = await axios.get("/calcData.json"); // JSON 데이터를 제공하는 API 엔드포인트 URL로 변경해야 합니다.
@@ -54,6 +14,50 @@ export default {
       } catch (error) {
         console.error("Error fetching JSON data:", error);
       }
+      let dsconfig = {
+        data: jsonData.value,
+        groups: [
+          { field: "class", isOpen: true },
+          { field: "math", isOpen: true },
+        ],
+      };
+
+      let gridConfig = {
+        dataSource: dsconfig,
+        container: "#grid",
+        width: "40%",
+        height: "400px",
+
+        columns: [
+          { field: "class", width: 80 },
+          { field: "name" },
+          { field: "number" },
+          {
+            field: "korean",
+            groups: [
+              {
+                group: "*",
+                aggregates: [{ func: "total", payload: "name" }],
+                header: { prefix: "합계: ", field: "total" },
+              },
+            ],
+            //   width: 50,
+          },
+          { field: "english", width: 50 },
+          {
+            field: "math",
+            groupTitle: {
+              title: { prefix: "수학점수", suffix: " 점", field: "math" },
+            },
+            width: 50,
+          },
+        ],
+        groupable: true,
+        groupAutoOpen: false,
+        navigatable: true,
+      };
+      const datagrid = SBGrid3.createGrid(gridConfig);
+      datagrid.refresh();
     });
   },
 };
